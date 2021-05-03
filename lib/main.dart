d161983b71fe9d0ccef0624a3fdc3ed9278af589
David Green <greensopinion@gmail.com>
2021-05-02 20:16:22 -0700

    initial scaffolding

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Maps Demo',
+      theme: ThemeData.light(),
+      home: MapPage(),
+    );
+  }
+}
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Maps Example")),
+        body: SafeArea(
+            child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Center(child: Text("Maps Example")))));
+  }
+}
